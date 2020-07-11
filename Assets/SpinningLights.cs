9dc124a39a6463be1759f10807aef7855e2cc0c3
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2020-07-11 23:15:52 +0200

    Greybox continued

---
+++ Assets/SpinningLights.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningLights : MonoBehaviour
+{
+    public float spinSpeed = 20f;
+
+
+    void Update()
+    {
+        transform.localEulerAngles += Vector3.up * spinSpeed * Time.deltaTime;
+    }
+}
